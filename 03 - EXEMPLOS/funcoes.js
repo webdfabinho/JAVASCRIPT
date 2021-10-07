f2b1e2c3500e65d58a568d7b95d343b08ca8266b
webdfabinho <webdfabinho@gmail.com>
2021-10-07 16:41:39 -0300

    Exemplos de javascript

--- 03 - EXEMPLOS/funcoes.js
+++ 03 - EXEMPLOS/funcoes.js
@@ -1,25 +1,26 @@
 /* Atividade 1: Alunos Aprovados
-Crie uma função que recebe o array alunos e um número que irá representar a média 
-final; Percorra o array e popule um novo array auxiliar apenas com os alunos 
+1 Crie uma função que recebe o array alunos e um número que irá representar a média 
+final; 
+2 Percorra o array e popule um novo array auxiliar apenas com os alunos 
 cujas notas são maiores ou iguais à média final;
-Utilize a técnica "object destructuring" para manipular as propriedades desejadas 
+3 Utilize a técnica "object destructuring" para manipular as propriedades desejadas 
 de cada aluno. */
 
 // solução 01
 
 const alunos = [
 	{
-		nome: 'João',
+		nome: 'Carol',
 		nota: 5,
 		turma: '1B',
 	},
 	{
-		nome: 'Sofia',
+		nome: 'Fabio',
 		nota: 9,
 		turma: '1B',
 	},
 	{
-		nome: 'Paulo',
+		nome: 'Gustavo',
 		nota: 6,
 		turma: '2C',
 	},
@@ -30,6 +31,52 @@ function alunosAprovados(alunos, media) {
 
 	for (let i = 0; i < alunos.length; i++) {
 		let { nota, nome } = alunos[i];
+
+		if (nota >= media) {
+			aprovados.push(nome);
+		}
+	}
+
+	return aprovados;
+}
+
+console.log(alunosAprovados(alunos, 5));
+
+// solução 2
+
+const alunos = [
+	{
+		nome: 'Carol',
+		nota: 5,
+		turma: '1B',
+	},
+	{
+		nome: 'Fabio',
+		nota: 9,
+		turma: '1B',
+	},
+	{
+		nome: 'Gustavo',
+		nota: 6,
+		turma: '2C',
+	},
+    {
+		nome: 'Mayara',
+		nota: 3,
+		turma: '2C',
+	},
+    {
+		nome: 'Rebeca',
+		nota: 4,
+		turma: '2C',
+	},
+];
+
+function alunosAprovados(arr, media) {
+	let aprovados = [];
+
+	for (let i = 0; i < arr.length; i++) {
+		const {nota, nome} = arr[i];
 
 		if (nota >= media) {
 			aprovados.push(nome);

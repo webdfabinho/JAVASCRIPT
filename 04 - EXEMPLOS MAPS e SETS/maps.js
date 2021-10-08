bb1488e448bd645d05be6ded06351d4d487c5137
webdfabinho <webdfabinho@gmail.com>
2021-10-08 08:58:25 -0300

    Exemplos de Javascript

---
+++ 04 - EXEMPLOS MAPS e SETS/maps.js
@@ -0,0 +1,36 @@
+/* Crie uma função que retorna o nome dos membros de um Map que tem o papel 
+'ADMIN' no sistema.
+
+1 - Crie uma função getAdmins que recebe um Map;
+2 - Crie um Map e popule-o com nomes de usuários 
+e seus papeis no sistema. (Ex: 'Luiz' => 'Admin');
+3 - Dentro de getAdmins, utilize o loop for...of 
+para retornar uma lista com os nomes dos usu;arios 
+que são administradores. */
+
+// solução 1
+
+const getAdmins = (map) => {
+	let admins = [];
+
+	for ([key, value] of map) {
+		if (value === 'ADMIN') {
+			admins.push(key);
+		}
+	}
+
+	return admins;
+};
+
+const userRoles = new Map();
+
+userRoles.set('Stephany', 'SUDO');
+userRoles.set('Luiz', 'ADMIN');
+userRoles.set('Elvira', 'ADMIN');
+userRoles.set('Carolina', 'USER');
+userRoles.set('Guilherme', 'USER');
+
+console.log(getAdmins(userRoles));
+
+// solução 2
+
---
+++ 04 - EXEMPLOS MAPS e SETS/sets.js
@@ -0,0 +1,13 @@
+/* Dado o array [30, 30, 40, 5, 223, 2049, 3034, 5], 
+retorne outro array apenas com valores únicos. */
+
+// Solução 1
+
+function uniqueElements(array) {
+	let unique = new Set(array);
+	return [...unique];
+}
+
+const arr = [30, 30, 40, 5, 223, 2049, 3034, 5];
+
+console.log(uniqueElements(arr));

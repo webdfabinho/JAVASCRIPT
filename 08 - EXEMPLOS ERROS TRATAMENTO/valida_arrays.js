2441ec9e9035e81f865f58fc10dc9a6ddecc7504
webdfabinho <webdfabinho@gmail.com>
2021-10-11 11:11:51 -0300

    Exemplos de javascript

--- 08 - EXEMPLOS ERROS TRATAMENTO/valida_arrays.js
+++ 08 - EXEMPLOS ERROS TRATAMENTO/valida_arrays.js
@@ -41,3 +41,43 @@ function validaArrays(arr, num) {
 }
 
 console.log(validaArrays([1, 2, 3], 0));
+
+// solução 2
+
+function validaArray (arr, num){
+try{
+    if (!arr && !num) throw new ReferenceError ("Envie os Parâmetros!");
+
+    if (typeof arr != 'object') throw new TypeError("Array precisa ser do tipo Object!");
+
+    if (typeof num != 'number') throw new TypeError("Array precisa ser do tipo Object!");
+
+    if (arr.length !== num) throw new RangeError("Tamanho invalido!");
+
+    return arr;
+
+}
+catch(e){
+    if (e instanceof ReferenceError){
+        console.log("Esse Erro é um ReferenceError!");
+        console.log(e.message);
+    }
+    else if (e instanceof TypeError){
+        console.log("Esse Erro é um TypeError!");
+        console.log(e.message);
+    }
+    else if (e instanceof RangeError){
+        console.log("Esse Erro é um RamgeError!");
+        console.log(e.message);
+    }
+    else{
+        console.log("Tipo de erro não esperado!" + e);
+    }
+}
+} 
+
+//console.log(validaArray());
+//console.log(validaArray(5, 5));
+//console.log(validaArray([], 'a'));
+//console.log(validaArray([], 5));
+console.log(validaArray([1, 2, 3, 4, 5], 5));
